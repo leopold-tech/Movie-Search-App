58dfee7a001ccae54367ab512e96ebcea1b5fd2b
Leo Rand <leopold.rand@gmail.com>
2023-08-30 14:35:12 +0530

    Solved edge case - finding no match for input

--- index.js
+++ index.js
@@ -10,10 +10,25 @@ const fetchData = async (input) => {
             s: input
         }
     });
-    console.log(res);
+
+    if (res.data.Error) {
+        return [];
+    }
+    return res.data.Search;
 };
 
-const onInput = e => {
-    fetchData(e.target.value);
+const onInput = async (e) => {
+    const movieList = await fetchData(e.target.value);
+    console.log(movieList);
+
+    for (let movie of movieList) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <img src="${movie.Poster}"/>
+            <h2>${movie.Title}</h2>
+            <h3>${movie.Year}</h3>
+        `;
+        document.querySelector('.display').appendChild(div);
+    }
 };
 input.addEventListener('input', debounce(onInput, 800));
