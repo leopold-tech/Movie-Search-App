0cfcc0f434d4ba4bab7d06f985de4fdea022c9d9
Leo Rand <leopold.rand@gmail.com>
2023-08-31 17:34:30 +0530

    Refactored code to make it more modular

--- scripts/index.js
+++ scripts/index.js
@@ -2,71 +2,50 @@ import { createAutoSearch } from "./autoSearch.js";
 import { apiKey } from "./config.js";
 import { onMovieSelect } from "./fetchData.js";
 
-
 // Fetches data from API
 // onMovieSelect(movie);
 
+// Additional functions/ arguements for createAutoSearch()
+const autoSearchConfig = {
+    //renders autosearch options
+    renderOption(movie) {
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+        return `
+        <img src="${imgSrc}"/>
+        ${movie.Title} (${movie.Year})
+    `;
+    },
+    onOptionSelect(movie) {
+        onMovieSelect(movie);
+    }, 
+    inputValue(movie) {
+        return movie.Title;
+    }, 
+    async fetchData(input) {
+        const res = await axios.get('http://www.omdbapi.com/', {
+            params: {
+                apikey: apiKey,
+                s: input
+            }
+        });
+    
+        if (res.data.Error) {
+            return [];
+        }
+        return res.data.Search;
+    }
+};
+
 // Autocomplete widget to search results
 createAutoSearch({
-    root: document.querySelector('#left-autocomplete'), 
-    renderOption(movie) {                           //renders autosearch options
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-        return `
-        <img src="${imgSrc}"/>
-        ${movie.Title} (${movie.Year})
-    `;
-    },
-    onOptionSelect(movie) {
-        onMovieSelect(movie);
-    }, 
-    inputValue(movie) {
-        return movie.Title;
-    }, 
-    async fetchData(input) {
-        const res = await axios.get('http://www.omdbapi.com/', {
-            params: {
-                apikey: apiKey,
-                s: input
-            }
-        });
-    
-        if (res.data.Error) {
-            return [];
-        }
-        return res.data.Search;
-    }
+    ...autoSearchConfig, 
+    root: document.querySelector('#left-autocomplete')
 });
 
 createAutoSearch({
-    root: document.querySelector('#right-autocomplete'), 
-    renderOption(movie) {                           //renders autosearch options
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-        return `
-        <img src="${imgSrc}"/>
-        ${movie.Title} (${movie.Year})
-    `;
-    },
-    onOptionSelect(movie) {
-        onMovieSelect(movie);
-    }, 
-    inputValue(movie) {
-        return movie.Title;
-    }, 
-    async fetchData(input) {
-        const res = await axios.get('http://www.omdbapi.com/', {
-            params: {
-                apikey: apiKey,
-                s: input
-            }
-        });
-    
-        if (res.data.Error) {
-            return [];
-        }
-        return res.data.Search;
-    }
+    ...autoSearchConfig, 
+    root: document.querySelector('#right-autocomplete')
 });
+
 // Feeds HTML template to DOM
 // movieTemplate;
-
-
