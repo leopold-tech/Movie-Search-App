1127fe9116a1978b9c96e6ecf0706b8f614afff9
Leo Rand <leopold.rand@gmail.com>
2023-08-31 17:16:46 +0530

    Fixed bug after refactoring code

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -1,20 +1,6 @@
 import { apiKey } from "./config.js";
 import { movieTemplate } from "./movieTemplate.js";
 
-export const fetchData = async (input) => {
-    const res = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: apiKey,
-            s: input
-        }
-    });
-
-    if (res.data.Error) {
-        return [];
-    }
-    return res.data.Search;
-};
-
 export async function onMovieSelect(movie) {
     const movieDetails = await axios.get('http://www.omdbapi.com/', {
         params: {
